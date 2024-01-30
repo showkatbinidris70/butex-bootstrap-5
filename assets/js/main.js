7388e6e49a94869af15184a9a22e6c038b3513cc
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-01-30 09:41:30 +0600

    carousel added

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>',
 
     responsive: [
       {
