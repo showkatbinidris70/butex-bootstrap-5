39b2a8362f53e7052a88c2dd847ae1fafaa61456
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-02-27 16:30:03 +0600

    sss

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,28 +145,21 @@ $(document).ready(function () {
 
     responsive: [
       {
-        breakpoint: 574,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
         breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1920,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
