3e17efd091d50e650c943bd592cd519354bd99b8
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-03-04 11:13:07 +0600

    animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+//css scroll animation start
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+//css scroll animation end
+
 // slick slider CAMPUS FACILITIES start
 $(document).ready(function () {
   $(".latest-news-slider").slick({
